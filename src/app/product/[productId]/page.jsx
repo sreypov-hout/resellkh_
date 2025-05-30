8515738d8c4ef348933d0c01e4b9b1778277ac7f
Vidd-08 <echandevid096@gmail.com>
2025-05-30 11:26:34 +0700

    Upate

--- src/app/product/[productId]/page.jsx
+++ src/app/product/[productId]/page.jsx
@@ -1,13 +1,13 @@
 // src/app/product/page.jsx
 // This is a Server Component by default
 
-import ProductGallery from '../../components/product/ProductGallery';
-import ProductDetails from '../../components/product/ProductDetails';
-import SellerInfo from '../../components/product/SellerInfo';
-import Reviews from '../../components/product/Reviews';
-import MoreFromSeller from '../../components/product/MoreFromSeller';
-import OtherProducts from '../../components/product/OtherProducts';
-import ContactSellerHeader from '../../components/product/ContactSellerHeader';
+import ProductGallery from '../../../components/product/ProductGallery';
+import ProductDetails from '../../../components/product/ProductDetails';
+import SellerInfo from '../../../components/product/SellerInfo';
+import Reviews from '../../../components/product/Reviews';
+import MoreFromSeller from '../../../components/product/MoreFromSeller';
+import OtherProducts from '../../../components/product/OtherProducts';
+import ContactSellerHeader from '../../../components/product/ContactSellerHeader';
 // import Footer from './components/Footer';
 
 const ProductPage = () => {
